c4ed45373f6036becb574b97298a06dec330123b
prashantsharma80 <sharmaprashant1584@gmail.com>
2023-08-17 09:39:39 +0530

    subtract function added

--- second.cpp
+++ second.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int subtract(int a, int b) {
+    if (a > b) {
+        return (a-b);
+    } else {
+        return (b-a);
+    }
+}
+
 int main() {
     int a, b;
     cout << "Enter two numbers: ";
     cin >> a >> b;
+    int diff = subtract(a, b);
+    cout << diff << endl;
     return 0;
 }
