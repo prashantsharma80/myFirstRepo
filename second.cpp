dcfdd98d859233119662ab2a27a2e7d307f650fc
prashantsharma80 <sharmaprashant1584@gmail.com>
2023-08-17 09:37:25 +0530

    add function added

--- second.cpp
+++ second.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int add(int a, int b) {
+    return (a+b);
+}
+
 int main() {
     int a, b;
     cout << "Enter two numbers: ";
     cin >> a >> b;
+    int sum = add(a, b);
+    cout << sum << endl;
     return 0;
 }
